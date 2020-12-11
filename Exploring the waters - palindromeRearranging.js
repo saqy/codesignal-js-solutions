5f8934cca4017ee65560078e1149f8f49c078e34
Muhammad Saqib <muhammadsaqiib@gmail.com>
2020-12-11 09:26:35 -0500

    adds solution for palindromeRearranging

---
+++ Exploring the waters - palindromeRearranging.js
@@ -0,0 +1,14 @@
+function palindromeRearranging(inputString) {
+  const charCounts = [...inputString].reduce((counts, char) => {
+    counts[char] = counts[char] ? counts[char] + 1 : 1;
+    return counts;
+  }, {});
+
+  return (
+    Object.values(charCounts).filter((count) => count % 2 !== 0).length <= 1
+  );
+}
+
+function isPlaindrome(str) {
+  return str.split("").reverse().join("") === str;
+}
