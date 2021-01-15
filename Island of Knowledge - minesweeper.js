3f231a224c62aab2ec77467d84c0f1f178ffc84c
Muhammad Saqib <muhammadsaqiib@gmail.com>
2021-01-14 22:42:48 -0500

    minesweeper in progress

--- Island of Knowledge - minesweeper.js
+++ Island of Knowledge - minesweeper.js
@@ -5,23 +5,48 @@ function minesweeper(matrix) {
   for (let i = 0; i < matrix.length; i++) {
     mineCounts = [];
     for (let j = 0; j < matrix[i].length; j++) {
-      if (matrix[i][j + 1] === true) {
-        count++;
-        mineCounts.push(count);
+      count = 0;
+      if (matrix[i][j + 1] !== undefined) {
+        if (matrix[i][j + 1] === true) {
+          count++;
+          mineCounts.push(count);
+        }
       }
-      if (matrix[i][j - 1] === true) {
-        count++;
-        mineCounts.push(count);
+      if (matrix[j - 1] !== undefined) {
+        if (matrix[i][j - 1] === true) {
+          count++;
+          mineCounts.push(count);
+        }
       }
-      if (matrix[i + 1][j] === true) {
-        count++;
-        mineCounts.push(count);
+      if (matrix[i + 1] !== undefined) {
+        if (matrix[i + 1][j] === true) {
+          count++;
+          mineCounts.push(count);
+        }
       }
-      if (matrix[i - 1][j] === true) {
-        count++;
-        mineCounts.push(count);
+      if (matrix[i - 1] !== undefined) {
+        if (matrix[i - 1][j] === true) {
+          count++;
+          mineCounts.push(count);
+        }
+      }
+
+      if (matrix[i + 1] !== undefined) {
+        if (matrix[i + 1][j + 1] === true) {
+          count++;
+          mineCounts.push(count);
+        }
+      }
+
+      if (matrix[j + 1] !== undefined) {
+        if (matrix[i + 1][j - 1] === true) {
+          count++;
+          mineCounts.push(count);
+        }
       }
     }
+    console.log(mineCounts);
     result.push(mineCounts);
   }
+  return result;
 }
