02ff2b20fb06baecba24c8fd61da20261733c28b
Muhammad Saqib <muhammadsaqiib@gmail.com>
2021-01-18 11:42:46 -0500

    adds solution for minesweeper

--- Island of Knowledge - minesweeper.js
+++ Island of Knowledge - minesweeper.js
@@ -1,52 +1,64 @@
 function minesweeper(matrix) {
-  let count = 0;
   let mineCounts = [];
-  let result = [];
   for (let i = 0; i < matrix.length; i++) {
-    mineCounts = [];
+    mineCounts.push([]);
     for (let j = 0; j < matrix[i].length; j++) {
-      count = 0;
+      mineCounts[i][j] = 0;
+      //above
+
+      if (matrix[i][j - 1] === true) {
+        mineCounts[i][j]++;
+      }
+
+      //below
       if (matrix[i][j + 1] !== undefined) {
         if (matrix[i][j + 1] === true) {
-          count++;
-          mineCounts.push(count);
+          mineCounts[i][j]++;
         }
       }
-      if (matrix[j - 1] !== undefined) {
-        if (matrix[i][j - 1] === true) {
-          count++;
-          mineCounts.push(count);
-        }
-      }
-      if (matrix[i + 1] !== undefined) {
-        if (matrix[i + 1][j] === true) {
-          count++;
-          mineCounts.push(count);
-        }
-      }
+      //left
       if (matrix[i - 1] !== undefined) {
         if (matrix[i - 1][j] === true) {
-          count++;
-          mineCounts.push(count);
+          mineCounts[i][j]++;
         }
       }
+
+      //right
+      if (matrix[i + 1] !== undefined) {
+        if (matrix[i + 1][j] === true) {
+          mineCounts[i][j]++;
+        }
+      }
+
+      //down right
 
       if (matrix[i + 1] !== undefined) {
         if (matrix[i + 1][j + 1] === true) {
-          count++;
-          mineCounts.push(count);
+          mineCounts[i][j]++;
         }
       }
 
-      if (matrix[j + 1] !== undefined) {
+      // down left
+      if (matrix[i + 1] !== undefined) {
         if (matrix[i + 1][j - 1] === true) {
-          count++;
-          mineCounts.push(count);
+          mineCounts[i][j]++;
+        }
+      }
+
+      //top right
+      if (matrix[i - 1] !== undefined) {
+        if (matrix[i - 1][j + 1] === true) {
+          mineCounts[i][j]++;
+        }
+      }
+
+      //top left
+      if (matrix[i - 1] !== undefined) {
+        if (matrix[i - 1][j - 1] === true) {
+          mineCounts[i][j]++;
         }
       }
     }
-    console.log(mineCounts);
-    result.push(mineCounts);
   }
-  return result;
+  return mineCounts;
 }
